c4c90ae444cf8a4657aef91b2d2d0f3a8cb8c5d3
Oleksandr_Balkashyn <skyaleksandr@gmail.com>
2021-03-14 00:49:10 +0200

    # Discord client

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+import os
+from dotenv import load_dotenv
+import logging
+
+load_dotenv()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
+
+file_handler = logging.FileHandler('log-file.txt')
+file_handler.setLevel('DEBUG')
+
+logger.addHandler(file_handler)
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    logger.debug(message)
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Fuck you!')
+
+
+token = os.getenv("TOKEN")
+client.run(token)
