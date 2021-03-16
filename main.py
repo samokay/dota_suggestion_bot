4d5d6535f5dd1edac5d742ca58a391ac2143bbfe
Oleksandr <skyaleksandr@gmail.com>
2021-03-16 10:13:41 +0200

    ob/refactor init commit (#3)
    Co-authored-by: samokay <54929787+samokay@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
 
-file_handler = logging.FileHandler('log-file.txt')
+file_handler = logging.FileHandler('log-file.txt', mode='w')
 file_handler.setLevel('DEBUG')
 
 logger.addHandler(file_handler)
@@ -16,20 +16,27 @@ logger.addHandler(file_handler)
 client = discord.Client()
 
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
+class DotaClient(discord.Client):
+
+    def __init__(self, token):
+        super().__init__()
+        self.token = token
+
+    async def on_ready(self):
+        logger.info(f"We have logged in as {self.user.name}")
+        logger.debug(self)
+
+    async def on_message(self, message):
+        logger.debug(message)
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Fuck you!')
+
+    def run(self):
+        super().run(self.token)
 
 
-@client.event
-async def on_message(message):
-    logger.debug(message)
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Fuck you!')
-
-
-token = os.getenv("TOKEN")
-client.run(token)
+bot = DotaClient(os.getenv("TOKEN"))
+bot.run()
